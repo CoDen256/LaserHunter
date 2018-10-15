1ef784ed5d1874fac5f23c09f4aae9c6510c4bd7
coden256 <den.blackshov@gmail.com>
2018-10-15 22:30:52 +0300

    initial

---
+++ android/src/com/mygdx/laserhunter/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.laserhunter;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.laserhunter.LaserHunterGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LaserHunterGame(), config);
+	}
+}
